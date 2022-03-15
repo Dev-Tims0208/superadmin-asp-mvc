04f3d95c1888cda29049b66c925a630bcc43210b
Dev-Tims0208 <eroy.timothy@gmail.com>
2022-03-15 10:40:01 +0800

    - initiate role assignment models

---
+++ efb-admin/Models/AppModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace efb_admin.Models
+{
+    public class RoleAssignment
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool isChecked { get; set; }
+    }
+
+    public class UserRoleViewModel
+    {
+        // constructor 
+        // instantiate props
+        public UserRoleViewModel()
+        {
+            this.UserRoles = new List<RoleAssignment>();
+        }
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+
+        public List<RoleAssignment> UserRoles { get; set; }
+    }
+}
