ecbf34dbb60d041f60e83ca5441d21d06d31011f
Dev-Tims0208 <eroy.timothy@gmail.com>
2022-03-15 10:23:55 +0800

    Add project files.

---
+++ efb-admin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(efb_admin.Startup))]
+namespace efb_admin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
